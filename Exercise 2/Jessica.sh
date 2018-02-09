db173d02dae1fe4860bc5f5cd74cbe660f289d21
jessicafaust <35717268+jessicafaust@users.noreply.github.com>
2018-02-08 21:51:40 -0900

    Update Jessica.sh

--- Exercise 2/Jessica.sh
+++ Exercise 2/Jessica.sh
@@ -23,4 +23,22 @@ sed -n '2~4p' SRR5170066_1.fastq > def
 #join line 1 and line 2 separated by a tab 
 paste abc def > SRR5fakefasta 
 
+#replace all "@" with ">" in prefex
+cat SRR5fakefasta | tr "@" ">" | less -S | > fakefasta
 
+# sort by sequence identifire
+sort fakefasta > fakefasta.sort.fa
+
+# get unique sequences 
+while read myline; do 
+while> echo $myline >> fakefasta.dup
+while> echo " " >> fakefasta.dup
+while> echo $myline >> fakefasta.dup
+while> echo " " >> fakefasta.dup
+done < fakefasta.sort.fa
+
+#count unique sequences
+grep ">" fakefasta.dup | uniq > fakefasta.uniq.fa
+cut -d ' ' -f2 fakefasta.uniq.fa | sort | uniq | wc -l > ex2answer.txt
+cat ex2answer.txt
+# 50622 unique sequences
