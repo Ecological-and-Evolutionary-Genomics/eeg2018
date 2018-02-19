b482bfa54694a85fa18e913ed8539b229428ce93
Alexis Walker <amwalker8@alaska.edu>
2018-02-19 14:48:50 -0900

    AWalk_E3.sh
    Still running assembly

---
+++ Exercise 3/AWalk_E3.sh
@@ -0,0 +1,14 @@
+#1 Find an Illumina MiSeq bacterial dataset SRA NCBI
+#psychromonas arcticus - hiseq not miseq
+# SRR1587406
+
+#login to PATRIC
+#Click on "Services" tab on upper toolbar
+#In drop-down menu click on "Assembly" under "Genome"
+#upload genome sequences to PATRIC via SRA run accession # -> paste # in box -> click arrow
+#you will then see accession # in "selected libraries"
+
+#2 Assemble the genome on PATRIC. 
+#chose the full spades assembly strategy
+#kept default min contig length (300bp) and coverage (5.0)
+#assembly is currently running ...
